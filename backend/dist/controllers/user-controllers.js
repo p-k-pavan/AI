12fe609ab774affce6e65b4f65f4a851a1b5507a
p-k-pavan <pkpavan2003@email.com>
2024-08-05 17:40:19 +0530

    Creating our first GET Request to get all users

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,3 +1,11 @@
-export const getAllUsers = () => {
+import User from "../models/User.js";
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "ok", users });
+    }
+    catch (error) {
+        return res.status(401).json({ message: "Error", error });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
